2e588a4d79edfe8b08155cf6d47437f91f3e8e9f
simon-an <26556185+simon-an@users.noreply.github.com>
2023-07-28 16:03:59 +0200

    initial commit
    Signed-off-by: simon-an <26556185+simon-an@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # rust-example-redis-federated-identities
+
+## Showcase to demonstrate the combination of Redis and Federated Identities
+
+see: https://learn.microsoft.com/en-us/azure/azure-cache-for-redis/cache-rust-get-started
+and here: https://learn.microsoft.com/de-de/azure/azure-cache-for-redis/cache-azure-active-directory-for-authentication#configure-your-redis-client-to-use-azure-active-directory
+
+Tested with gitlab federated identities and Azure Redis Cache 
+
+https://docs.gitlab.com/ee/ci/cloud_services/azure/
+
+```yaml
+test:
+  stage: .post
+  image: rust:latest
+  variables:
+    REDIS_HOSTNAME: "<YOUR_CLUSTER>.cache.windows.net:6380"
+    AZURE_SP_OBJECT_ID: <THE SERVICE PRINCIPLE OBJECT ID>
+  id_tokens:
+    AZURE_FEDERATED_TOKEN:
+      aud: https://git.eon-cds.de
+  script:
+    - cd test
+    - cargo run --release
+```
+
+TODOs: 
+ - test with github + and azure ad
+ - test with k8s workload identity
---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use log::info;
+use redis::Commands;
+use std::{env, sync::Arc};
+
+use azure_core::auth::TokenCredential;
+use azure_identity::DefaultAzureCredentialBuilder;
+
+async fn connect(sp_object_id: String) -> redis::Connection {
+    let creds: Arc<azure_identity::DefaultAzureCredential> = Arc::new(
+        DefaultAzureCredentialBuilder::new()
+            // .exclude_environment_credential()
+            // .exclude_azure_cli_credential()
+            // .exclude_managed_identity_credential()
+            .build(),
+    );
+    let token = creds
+        .get_token("acca5fbb-b7e4-4009-81f1-37e38fd66d78")
+        .await // This is the azure resource id for redis
+        .unwrap();
+    let redis_host_name =
+        env::var("REDIS_HOSTNAME").expect("missing environment variable REDIS_HOSTNAME");
+    info!("we have a token and will use it with sp: {} for {redis_host_name}", sp_object_id);
+
+    let redis_conn_url = format!(
+        "rediss://{sp_object_id}:{}@{}",
+        token.token.secret(),
+        redis_host_name
+    );
+
+    redis::Client::open(redis_conn_url)
+        .expect("invalid connection URL")
+        .get_connection()
+        .expect("failed to connect to redis")
+}
+
+fn basics(conn: &mut redis::Connection) {
+    let _: () = redis::cmd("SET")
+        .arg("foo")
+        .arg("bar")
+        .query(conn)
+        .expect("failed to execute SET for 'foo'");
+
+    let bar: String = redis::cmd("GET")
+        .arg("foo")
+        .query(conn)
+        .expect("failed to execute GET for 'foo'");
+    println!("value for 'foo' = {}", bar);
+
+    let _: () = conn
+        .incr("counter", 2)
+        .expect("failed to execute INCR for 'counter'");
+    let val: i32 = conn
+        .get("counter")
+        .expect("failed to execute GET for 'counter'");
+    println!("counter = {}", val);
+}
+
+fn list(conn: &mut redis::Connection) {
+    let list_name = "items";
+
+    let _: () = redis::cmd("LPUSH")
+        .arg(list_name)
+        .arg("item-1")
+        .query(conn)
+        .expect("failed to execute LPUSH for 'items'");
+
+    let item: String = conn
+        .lpop(list_name, None)
+        .expect("failed to execute LPOP for 'items'");
+    println!("first item: {}", item);
+
+    let _: () = conn.rpush(list_name, "item-2").expect("RPUSH failed");
+    let _: () = conn.rpush(list_name, "item-3").expect("RPUSH failed");
+
+    let len: isize = conn
+        .llen(list_name)
+        .expect("failed to execute LLEN for 'items'");
+    println!("no. of items in list = {}", len);
+
+    let items: Vec<String> = conn
+        .lrange(list_name, 0, len - 1)
+        .expect("failed to execute LRANGE for 'items'");
+
+    println!("listing items in list");
+    for item in items {
+        println!("item: {}", item)
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    env_logger::init();
+    let sp_object_id =
+        env::var("AZURE_SP_OBJECT_ID").expect("Missing AZURE_SP_OBJECT_ID environment variable.");
+    let mut connection = connect(sp_object_id).await;
+    info!("connected to redis");
+    info!("basic()");
+    basics(&mut connection);
+    info!("list()");
+    list(&mut connection);
+}
